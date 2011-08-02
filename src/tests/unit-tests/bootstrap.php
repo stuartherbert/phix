05e2a94c979f5b62e324d84c7e0bf7a22b3d46fd
Stuart Herbert <stuart@stuartherbert.com>
2011-08-02 18:27:00 +0100

    Updated bootstrap file for unit testing

--- src/tests/unit-tests/bootstrap.php
+++ src/tests/unit-tests/bootstrap.php
@@ -6,27 +6,27 @@
 //		A helping hand for running our unit tests
 //
 // Author	Stuart Herbert
-//		(stuart.herbert@gradwell.com)
+//		(stuart@stuartherbert.com)
 //
-// Copyright	(c) 2010 Gradwell dot com Ltd
+// Copyright	(c) 2011 Stuart Herbert
+// 		(c) 2010 Gradwell dot com Ltd
 //		All rights reserved
 //
 // =========================================================================
 
-// step 1: create the APP_TOPDIR constant that all MF components require
+// step 1: create the APP_TOPDIR constant that all components require
 define('APP_TOPDIR', realpath(__DIR__ . '/../../php'));
 define('APP_LIBDIR', realpath(__DIR__ . '/../../../vendor/php'));
 define('APP_TESTDIR', realpath(__DIR__ . '/php'));
 
-// step 2: find the autoloader, and install it
-require_once(APP_LIBDIR . '/gwc.autoloader.php');
-
-// step 3: add the lib-vendor code to the include path
+// step 2: add the lib-vendor code to the include path
 set_include_path(APP_LIBDIR . PATH_SEPARATOR . get_include_path());
 
-// step 4: add the tests folder to the include path
+// step 3: add the tests folder to the include path
 set_include_path(APP_TESTDIR . PATH_SEPARATOR . get_include_path());
 
-// step 5: add our code to the include path
+// step 4: add our code to the include path
 set_include_path(APP_TOPDIR . PATH_SEPARATOR . get_include_path());
 
+// step 5: find the autoloader, and install it
+require_once(APP_LIBDIR . '/psr0.autoloader.php');
